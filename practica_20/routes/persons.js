96f0d0d35b7586679ac74926047724b9ed067baf
ttejeda <ttejeda@ucol.mx>
2022-05-20 07:47:33 -0500

    copied practica_19 code, installed packages and adapted route person.js

---
+++ practica_20/routes/persons.js
@@ -0,0 +1,30 @@
+//Inyectar dependencias.
+const express = require('express');
+let router = express.Router();
+const mongoose = require('mongoose');
+const Person = require('../models/person.js');
+
+//Definir una ruta donde se imprimirá el objeto json.
+router.get('/persons', function(req, res, next){
+    Person.find(function(err, persons){
+        if(err) return next(err); //Regresa un error.
+        res.json(persons); //Se regresa la información que pedimos.
+    });
+});
+
+router.get('/person', function(req, res){
+    res.render('person');
+});
+
+router.post('/addPerson', function(req, res){
+    let myPerson = new Person({
+        nombre: req.body.nombre,
+        edad: req.body.edad,
+        tipoSangre: req.body.tipoSangre,
+        nss: req.body.nss
+    });
+    myPerson.save();
+});
+
+//Exportar el módulo.
+module.exports = router;
